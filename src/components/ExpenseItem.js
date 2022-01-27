33993e594f8bf5365597cc3c6ca74f88961d315d
salvador salazar <salvador.g.salazar79@gmail.com>
2022-01-26 22:09:29 -0600

    react practice

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+function ExpenseItem(){
+    return(
+        <div>
+            <div>
+                March 28th,2022
+            </div>
+            <div>
+                <h2>Car insurance</h2>
+                <div>$135.00</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
